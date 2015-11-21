555496793213f1934154a1496012a8d68932702e
MAriaADF <hellenl2012@gmail.com>
2015-11-21 16:32:47 -0600

    commit # 2
    hecho 21/11/2015

--- FilaBanco/src/filabanco/Caja.java
+++ FilaBanco/src/filabanco/Caja.java
@@ -10,5 +10,25 @@ package filabanco;
  * @author Hellen Lopez A y Daniel Murillo 
  */
 public class Caja {
+        //atributos de la clase
+    public Lista personas;
+    public String nombre;
+    private boolean ocupado;
+    
+//metodo constructor de la clase
+    public Caja(String pNombre) {
+        this.ocupado = false;
+        personas = new Lista();
+        nombre = pNombre;
+    }
+//metodos set y get de los atributos de la clase
+    public boolean cajeroOcupado() {//Método que indica que el cajero se encuetra ocupado
+     return ocupado;
+    }
+
+    public void setcajeroOcupado(boolean ocupado) {//entra un dato donde indica si el cajero esta ocupado o no
+        this.ocupado = ocupado;
+    } 
+}
     
 }
