223f4ba43d6f2ff5376460c6397b3e5ef073ffb7
danimurillo <moder_dani@hotmail.es>
2015-11-25 21:11:45 -0600

    Commit # 3 se agregan los metodos de crear la fila y el tiempo promedio de los cajeros al atender

--- FilaBanco/src/filabanco/Banco.java
+++ FilaBanco/src/filabanco/Banco.java
@@ -42,13 +42,64 @@ public class Banco {
         
     }
     //------//donde se crea la fila, se da la prioridad y se elige el tiempo de atencion------------------------------
+  
+    /**
+     * Método que crea la Fila
+     * @param cl: Variable de Tipo Cliente
+     */
     
-    public void crearFila(String Personas){ 
+    public void crearFila(Personas cl){ 
+    int prio = (int) (Math.random() * 3 + 1);
+        int tiemp = (int) (Math.random() * (11 - 2) + 2);
+        int nombre = (int) (Math.random() * 9 + 1);
+        switch (prio) { //Orden de Prioridades
+            case 1:
+                cl.setPrioridad('A');
+                cl.setNumFila(contador);
+                cl.setNombre(this.nom[nombre]);
+                contador++;
+                break;
+            case 2:
+                cl.setPrioridad('B');
+                cl.setNumFila(contador);
+                while (nombre != 2 && nombre != 3 && nombre != 5 && nombre != 9) {
+                    nombre = (int) (Math.random() * 9 + 1);
+                }
+                cl.setNombre(this.nom[nombre]);
+                contador++;
+                break;
+            case 3:
+                cl.setPrioridad('C');
+                cl.setNumFila(contador);
+                cl.setNombre(this.nom[nombre]);
+                contador++;
+                break;
+        }
+        cl.setTiempoTramite(tiemp);
+        fila.insertarDato(cl); //Agrega Clientes a la Fila
     }
     
     //--------------------------se da el tiempo de atencion de cada caja-----------------------------------------------
-    public String promedioCajero(){
-       return "";
+      //metodo que retorna los usuarios atendidos por cada cajero y el tiempo total
+    /**
+     * Método que promedia el tiempo de atención de las cajas
+     * @param caj: Variable de Tipo "Caja"
+     * @return: Retorna los datos ordenados sobre la atención en la caja
+     */
+    
+    public String promedioCajero(Caja caj){
+        
+         String retorno = "";
+        int tiempoTotal = 0;
+        Nodo temp = caj.personas.primero;
+        for (int i = 0; i < caj.personas.tamaño; i++) {
+            tiempoTotal = tiempoTotal + temp.dato.getTiempoTramite();
+            temp = temp.siguiente;
+        }
+        retorno =  caj.nombre + "\n" + " Total de clientes atendidos : " + caj.personas.tamaño + "\n" + " Tiempo total : "
+                + tiempoTotal + " minutos" + "\n" + "Promedio : " + (tiempoTotal / caj.personas.tamaño) + " cliente" + "\n\n";
+        return (retorno);
+      
  }
     //------------------------------ordena las personas por prioridad--------------------------------------------------
     public void ordenarFia(){
